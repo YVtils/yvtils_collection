b78c270fc5a8359f00088f3a34cbbeb76708c9c5
WolfiiYV <minecraftwandler@gmail.com>
2025-05-20 14:46:37 +0200

    Change paperweight userdev version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.1.21" apply false
     kotlin("plugin.serialization") version "2.1.21" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.userdev") version "2.0.0-SNAPSHOT" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" apply false
     id("xyz.jpenilla.run-paper") version "2.3.1" apply false
 }
 
