a9b8342c349a3a12704ea0251cbfe09b92a0499b
WolfiiYV <minecraftwandler@gmail.com>
2025-06-07 00:20:28 +0200

    Update regions things

--- regions-core/build.gradle.kts
+++ regions-core/build.gradle.kts
@@ -5,8 +5,21 @@ dependencies {
     implementation(project(":regions"))
 }
 
+val version = "1.0.0-beta.2"
+
 tasks {
     runServer {
         minecraftVersion("1.21.5")
+    }
+
+    shadowJar {
+        archiveBaseName.set("YVtils-RG")
+        archiveVersion.set(version)
+        archiveClassifier.set("")
+        archiveFileName.set("YVtils-RG_v${version}.jar")
+
+        manifest {
+            attributes["Main-Class"] = "yv.tils.rg.YVtils"
+        }
     }
 }
