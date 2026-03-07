5b91dd46fbd823e9a7778fca20ba34d5691d44f1
Lyvric <lyvric@yvtils.net>
2026-03-07 19:13:54 +0100

    WIP: Add pvp control command

---
+++ essentials/src/main/kotlin/yv/tils/essentials/commands/handler/PvPHandler.kt
@@ -0,0 +1,100 @@
+/*
+ * Part of the YVtils Project.
+ * Copyright (c) 2026 Lyvric / YVtils
+ *
+ * Licensed under the Mozilla Public License 2.0 (MPL-2.0)
+ * with additional YVtils License Terms.
+ * License information: https://yvtils.net/license
+ *
+ * Use of the YVtils name, logo, or brand assets is subject to
+ * the YVtils Brand Protection Clause.
+ */
+
+package yv.tils.essentials.commands.handler
+
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import yv.tils.config.language.LanguageHandler
+import yv.tils.essentials.config.ConfigFile
+import yv.tils.essentials.language.LangStrings
+import yv.tils.utils.logger.Logger
+import yv.tils.utils.server.ServerUtils
+
+class PvPHandler {
+    fun handler(sender: CommandSender, state: Boolean? = null) {
+        val currentState = getPvPState()
+
+        if (state == null) {
+            handler(sender, !currentState)
+            return
+        }
+
+        if (state == currentState) {
+            sender.sendMessage(
+                LanguageHandler.getMessage(
+                    LangStrings.COMMAND_PVP_ALREADY,
+                    sender
+                )
+            )
+            return
+        }
+
+        setPvPState(state)
+
+        sender.sendMessage(
+            LanguageHandler.getMessage(
+                if (state) LangStrings.COMMAND_PVP_ENABLE else LangStrings.COMMAND_PVP_DISABLE,
+                sender
+            )
+        )
+
+        if (sender is Player) {
+            Logger.info("PvP state changed to ${if (state) "enabled" else "disabled"} by ${sender.name}")
+        }
+    }
+
+    fun checkStateHandler(sender: CommandSender) {
+        val currentState = getPvPState()
+
+        sender.sendMessage(
+            LanguageHandler.getMessage(
+                if (currentState) LangStrings.COMMAND_PVP_STATE_ENABLED else LangStrings.COMMAND_PVP_STATE_DISABLED,
+                sender
+            )
+        )
+    }
+
+    fun onDamage(e: EntityDamageByEntityEvent) {
+        // TODO: Add bypass perm
+        if (getPvPState()) return
+
+        if (e.entity is Player && e.damager is Player) {
+            e.isCancelled = true
+            e.damager.sendActionBar(
+                LanguageHandler.getMessage(
+                    LangStrings.PVP_DISABLED,
+                    e.damager
+                )
+            )
+        }
+    }
+
+    fun getPvPState(): Boolean {
+        return ServerUtils.isPvPEnabled
+    }
+
+    fun setPvPState(state: Boolean) {
+        ServerUtils.isPvPEnabled = state
+        savePvPState(state)
+    }
+
+    fun loadPvPState() {
+        ConfigFile.getBoolean("pvpState")?.let { setPvPState(it) }
+    }
+
+    fun savePvPState(state: Boolean) {
+        ConfigFile.set("pvpState", state)
+    }
+}
